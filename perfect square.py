f4ef68ccb5960725be74095746bac5a6e6b65f69
manisahithya <you@manisahithya8@gmail.com>
2021-11-19 14:50:07 +0530

    apps

---
+++ perfect square.py
@@ -0,0 +1,18 @@
+import math
+n=int(input())
+r=math.sqrt(n)
+if int(root+0.5)**2==num;
+	print("enter the perfect square");
+else:
+	print("enter the not perfect square");
+//square
+num=int(input("enter any number"))
+f=0
+for i in range(1,num);
+	if i*i==num;
+	f=1
+	break
+if f==1;
+	print("enter perfect square");
+else:
+	print("enter not perfect square");
